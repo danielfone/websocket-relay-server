2b3ba9d96407e471ebdbdf99c6ca0124a4d908b5
Daniel Fone <daniel@fone.net.nz>
2023-03-14 21:33:05 +1300

    Simplify worker

--- src/index.ts
+++ src/index.ts
@@ -11,19 +11,10 @@ export interface Env {
 //
 // Connect to the server with a websocket request:
 //
-//  const ws = new WebSocket("wss://example.com/{channel_id}?create={true|false}");
-//
-// `create` is an optional paramter. If omitted, the connection will create a
-// new channel if it does not already exist, or join an existing channel if it
-// does. If provided, the connection will only create a new channel if the
-// value is `true`, or join an existing channel if the value is `false`,
-// otherwise it will fail.
+//  const ws = new WebSocket("wss://example.com/{channel_id}");
 //
 export default {
-  async fetch(
-    request: Request,
-    env: Env,
-  ): Promise<Response> {
+  async fetch(request: Request, env: Env): Promise<Response> {
     // Return an error if this is not a websocket request
     if (request.headers.get("Upgrade") !== "websocket") {
       return new Response("Expected Upgrade: websocket", {
@@ -31,13 +22,10 @@ export default {
       });
     }
 
-    // The path is expected to be in the form of /{channel_id}?create={true|false}
+    // The path is expected to be in the form of /{channel_id}
     // Check we have a channel_id and extract it.
     const url = new URL(request.url);
     const channelId = url.pathname.slice(1);
-    if (!channelId) {
-      return new Response("No channel specified.", { status: 404 });
-    }
 
     // Load the DurableObject for the given channel and route the request to it.
     const id = env.WSR.idFromName(channelId);
@@ -63,28 +51,21 @@ export class WebSocketRelay {
     // Get the connection parameters from the request.
     const url = new URL(request.url);
     this.channelId ??= url.pathname.slice(1);
-    const create = url.searchParams.get("create");
 
     const { 0: client, 1: server } = new WebSocketPair();
-    this.configureWebSocket(server, create);
+    this.configureWebSocket(server);
 
     return new Response(null, { status: 101, webSocket: client });
   }
 
-  private configureWebSocket(ws: WebSocket, create?: string | null) {
+  private configureWebSocket(ws: WebSocket) {
     const channelId = this.channelId;
     const clientId = Math.random().toString(36).slice(2); // Generate a random id for the client.
 
-		console.log("websocket.connect", { channelId, clientId, create });
+    console.log("websocket.connect", { channelId, clientId });
     ws.accept();
 
-    if (create === "true" && this.active) {
-      ws.close(4000, "Channel already exists");
-    } else if (create === "false" && !this.active) {
-      ws.close(4000, "Channel not found");
-    } else {
-			this.clients.add(ws);
-		}
+    this.clients.add(ws);
 
     ws.addEventListener("message", (event: MessageEvent) => {
       const byteSize = (event.data instanceof ArrayBuffer)
@@ -100,17 +81,17 @@ export class WebSocketRelay {
     ws.addEventListener("close", (event: CloseEvent) => {
       const { code, reason } = event;
       console.log("websocket.close", { channelId, clientId, code, reason });
-			// Remove the client from the set of active clients.
+      // Remove the client from the set of active clients.
       this.clients.delete(ws);
     });
 
-		ws.addEventListener("error", (event: ErrorEvent) => {
-			console.log("websocket.error", { channelId, clientId, error: event.error });
-			this.clients.delete(ws);
-		});
-  }
-
-  private get active() {
-    return this.clients.size > 0;
+    ws.addEventListener("error", (event: ErrorEvent) => {
+      console.log("websocket.error", {
+        channelId,
+        clientId,
+        error: event.error,
+      });
+      this.clients.delete(ws);
+    });
   }
 }
